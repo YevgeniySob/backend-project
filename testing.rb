8375d51f5139549121b7d0abd9f0cd8c87aaea97
Yevgeniy <yevgeniyerot@gmail.com>
2019-06-06 20:59:47 -0400

    dependancies

---
+++ testing.rb
@@ -0,0 +1,24 @@
+vegetables = ['Asparagus, Broccoli, Carrots, Cauliflower, Celery, Corn, Cucumbers, Lettuce, Mushrooms, Onions, Peppers, Potatoes  Spinach, Squash, Zucchini, Tomatoes']
+
+condiments_sauces = ['BBQ sauce', 'Gravy', 'Honey', 'Ketchup', 'Mustard', 'Mayonnaise', 'Pasta sauce', 'Relish', 'Salad dressing', 'Salsa', 'Soy sauce', 'Steak sauce', 'Syrup', 'Worcestershire sauce']
+
+dairy = ['Butter', 'Margarine', 'Cottage cheese', 'Half & half', 'Milk', 'Sour cream', 'Whipped cream', 'Yogurt']
+
+baked_goods = ['Bagels', 'Croissants', 'Buns', 'Cake', 'Cookies', 'Donuts', 'Pastries', 'bread', 'Pie', 'Pita bread', 'Sliced bread']
+
+fruits = ['Apples', 'Avocados', 'Bananas', 'Berries', 'Cherries', 'Grapefruit', 'Grapes', 'Kiwis', 'Lemons', 'Limes', 'Melon', 'Nectarines', 'Oranges', 'Peaches', 'Pears', 'Plums']
+
+various_groceries = ['Bouillon cubes', 'Cereal', 'Coffee Filters', 'Instant potatoes', 'Lemon juice',  'Lime juice', 'Mac & cheese', 'Olive oil', 'Packaged meals', 'Pancake Waffle mix', 'Pasta', 'Peanut butter', 'Pickles', 'Rice', 'Tea', 'Vegetable oil', 'Vinegar']
+
+meat = ['Bacon', 'Sausage', 'Beef', 'Chicken', 'Ground beef', 'Turkey', 'Ham', 'Pork', 'Lunchmeat', 'Turkey']
+
+baking = ['Baking powder', 'Baking Soda', 'Bread crumbs', 'Cake', 'Brownie mix', 'Cake icing', 'Decorations', 'Chocolate chips', 'Cocoa', 'Flour', 'Shortening' , 'Sugar' , 'Sugar substitute', 'Yeast']
+
+cheese = ['Blue cheese', 'Cheddar', 'Cottage cheese', 'Cream cheese', 'Feta', 'Goat cheese', 'Mozzarella' , 'Parmesan' , 'Provolone' , 'Ricotta', 'Swiss']
+
+# refrigerated = ['Eggs', English muffins, Fruit juice, Hummus , Ready-bake breads, Tofu, Tortillas]
+
+# Canned foods, Applesauce, Baked beans, Broth, Fruit, Olives, Tinned meats, Tuna / Chicken, Soup / Chili, Tomatoes, Veggies
+
+# Seafood, Catfish , Crab , Lobster , Mussels , Oysters , Salmon , Shrimp , Tilapia , Tuna
+
